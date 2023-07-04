1f69a8a78dc6fc4155d4e066bd855876c1d7f4a5
Boyeon Ihn <boyeonihn@gmail.com>
2023-07-05 06:53:54 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { DiaryEditor } from './DiaryEditor';
+
+function App() {
+  return (
+    <div>
+      <DiaryEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DiaryEditor.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export const DiaryEditor = () => {
+  const [author, setAuthor] = useState('');
+  const [rating, setRating] = useState();
+  const [content, setContent] = useState('');
+
+  const handleAuthorChange = (e) => {
+    const authorInput = e.target.value;
+    setAuthor(authorInput);
+  };
+
+  const handleRating = (e) => {
+    const ratingInput = e.target.value;
+    console.log(ratingInput);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="DiaryEditor">
+      <h1>펫 일기장</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleAuthorChange}
+          type="text"
+          placeholder="Author"
+          name="author"
+          value={author}
+        />
+        <textarea placeholder="Write your diary!"></textarea>
+        <label htmlFor="rating">Today's Rating</label>
+        <input id="rating" type="number" max="5" min="1"></input>
+        <button>일기 저장하기</button>
+      </form>
+    </section>
+  );
+};
