d9162d326978f4dd7c6c778d0a6a546e184689c4
Boyeon Ihn <boyeonihn@gmail.com>
2023-07-09 09:38:31 +0900

    feat: add conditional rendering of edit form per isEdit state

--- src/DiaryItem.jsx
+++ src/DiaryItem.jsx
@@ -1,4 +1,5 @@
 import styles from './DiaryItem.module.css';
+import { useState } from 'react';
 export const DiaryItem = ({
   author,
   content,
@@ -8,6 +9,20 @@ export const DiaryItem = ({
   deleteEntry,
   editEntry,
 }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+  const [localRating, setLocalRating] = useState(rating);
+  const handleContentChange = (event) => {
+    const content = event.target.value;
+    setLocalContent(content);
+  };
+
+  const handleRatingChange = (event) => {
+    const rating = event.target.value;
+    setLocalRating(rating);
+  };
   const onEdit = (event) => {
     const id = event.target.className;
     const entry = {
@@ -15,6 +30,7 @@ export const DiaryItem = ({
       rating: localRating,
     };
     editEntry(entry, id);
+    toggleIsEdit();
   };
 
   return (
@@ -23,20 +39,45 @@ export const DiaryItem = ({
         {isEdit ? '저장하기' : '수정하기'}
       </button>
       <div className={styles.info}>
-        <button
-          className={id}
-          onClick={(e) => {
-            const id = e.target.className;
-            deleteEntry(id);
-          }}
-        >
-          삭제하기
-        </button>
-        <h3>작성자: {author}</h3>
-        <small>감정 점수: {rating}</small>
+        <h4>작성자: {author}</h4>
+        <div className={styles.selectBox}>
+          {isEdit ? (
+            <>
+              <label htmlFor="rating">감정 점수</label>
+              <select
+                id="rating"
+                name="rating"
+                onChange={handleRatingChange}
+                value={rating}
+              >
+                <option value=""></option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+              </select>
+            </>
+          ) : (
+            <small>감정 점수: {rating}</small>
+          )}
+        </div>
+
         <h4>{new Date(createdAt).toLocaleString()}</h4>
       </div>
-      <p className={styles.content}>{content}</p>
+      <div className={styles.content}>
+        {isEdit ? (
+          <>
+            <textarea
+              name="content"
+              onChange={handleContentChange}
+              value={localContent}
+            />
+          </>
+        ) : (
+          content
+        )}
+      </div>
     </div>
   );
 };
